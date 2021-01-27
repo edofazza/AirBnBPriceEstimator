14c3caad8534cfcc42ffdc545c574c74fe70bc65
edofazza <fazzari.edoardo@gmail.com>
2021-01-27 16:04:16 +0100

    Java Project Added

---
+++ src/main/java/com/unipi/dmaml/airbnbpriceestimator/Main.java
@@ -0,0 +1,7 @@
+package com.unipi.dmaml.airbnbpriceestimator;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/unipi/dmaml/airbnbpriceestimator/csv/CSVHandler.java
@@ -0,0 +1,13 @@
+package com.unipi.dmaml.airbnbpriceestimator.csv;
+
+public class CSVHandler {
+    private String csvFileName;
+
+    public CSVHandler() {
+
+    }
+
+    public CSVHandler(String csvFileName) {
+        this.csvFileName = csvFileName;
+    }
+}
