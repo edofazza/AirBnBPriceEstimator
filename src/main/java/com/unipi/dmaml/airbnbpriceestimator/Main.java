9add6fae56e6df489da377d35d6292c2259bc898
edofazza <fazzari.edoardo@gmail.com>
2021-02-04 21:04:56 +0100

    HotVectorGenerator funzionante

--- src/main/java/com/unipi/dmaml/airbnbpriceestimator/Main.java
+++ src/main/java/com/unipi/dmaml/airbnbpriceestimator/Main.java
@@ -2,9 +2,12 @@ package com.unipi.dmaml.airbnbpriceestimator;
 
 import com.unipi.dmaml.airbnbpriceestimator.utils.HotVectorGenerator;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         HotVectorGenerator hotVectorGenerator = new HotVectorGenerator();
-        hotVectorGenerator.getAllFeatures("csv/amenities.csv");
+        List<String> features = hotVectorGenerator.getAllFeatures("csv/amenities.csv");
+        hotVectorGenerator.createHotVectorCSV("csv/result.csv", "csv/amenities.csv", features);
     }
 }
--- src/main/java/com/unipi/dmaml/airbnbpriceestimator/utils/HotVectorGenerator.java
+++ src/main/java/com/unipi/dmaml/airbnbpriceestimator/utils/HotVectorGenerator.java
@@ -1,6 +1,9 @@
 package com.unipi.dmaml.airbnbpriceestimator.utils;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +22,7 @@ public class HotVectorGenerator {
                 line = line.replace("]", "");
 
                 // split the data
-                String[] featureArray = line.split(",");
+                String[] featureArray = line.split(", ");
                 // add the feature if it’s not present yet
                 for (String s : featureArray) {
                     if (!featureSet.contains(s))
@@ -33,5 +36,45 @@ public class HotVectorGenerator {
         }
         
         return featureSet;
+    }
+
+    public void createHotVectorCSV(String newFilePath, String csv, List<String> featureSet) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(newFilePath), StandardOpenOption.APPEND);
+             BufferedReader br = new BufferedReader(new FileReader(csv))
+        ) {
+            // INSERT THE NEW COLUMN HEADERS
+            String[] featureArray = featureSet.toArray(new String[0]);
+            writer.write(String.join(",", featureArray) + "\n");
+
+            // GET EACH LINE AND CREATE THE NEW LINE FROM IT
+            String line;
+            while((line = br.readLine())!=null) {
+                // remove not useful chars
+                line = line.replace("\"", "");
+                line = line.replace("[", "");
+                line = line.replace("]", "");
+
+                // split the data
+                String[] features = line.split(", ");
+
+                // NEW STRING THAT WILL CONTAINS THE BINARY INFORMATION
+                String newString = new String();
+
+                // FOR EACH FEATURE CHECK IF THE BnB HAS IT
+                for (String s : featureSet) {
+                    for (String feature: features) {
+                        if (s.equals(feature)) {
+                            newString += "1,";
+                            break;
+                        }
+                    }
+                    newString += "0,";
+                }
+                newString = newString.substring(0, newString.length() - 1);
+                writer.write(newString + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
