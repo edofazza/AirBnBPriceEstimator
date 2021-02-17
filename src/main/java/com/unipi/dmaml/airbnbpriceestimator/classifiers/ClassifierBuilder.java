31ae182ad190eb441fbb0ee60a9248602996ebef
edofazza <fazzari.edoardo@gmail.com>
2021-02-17 17:16:19 +0100

    Create directories if not present

--- src/main/java/com/unipi/dmaml/airbnbpriceestimator/classifiers/ClassifierBuilder.java
+++ src/main/java/com/unipi/dmaml/airbnbpriceestimator/classifiers/ClassifierBuilder.java
@@ -4,9 +4,20 @@ import com.unipi.dmaml.airbnbpriceestimator.classifiers.algorithms.*;
 import com.unipi.dmaml.airbnbpriceestimator.classifiers.loaders.DatasetFromCsvLoader;
 import weka.core.Instances;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class ClassifierBuilder {
 
     public static void main(String[] args){
+        try {
+            Files.createDirectories(Paths.get("models/"));
+            Files.createDirectories(Paths.get("results/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Instances data = new DatasetFromCsvLoader().loadData();
         data.setClass(data.attribute("price"));
         //new LinearRegression(data).buildClassifiersAndSaveResults();
