579c38ad007dad5d431aae0ba98f67f87aa23ed3
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-02-18 10:16:36 +0100

    optimization on classifier building, test with M5Rules

--- src/main/java/com/unipi/dmaml/airbnbpriceestimator/application/prediction/Builder.java
+++ src/main/java/com/unipi/dmaml/airbnbpriceestimator/application/prediction/Builder.java
@@ -8,8 +8,9 @@ import weka.core.*;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class Builder {
-    private static final String selectedClassifier="RandomForest";
+    private static final String selectedClassifier="M5Rules";
     private static final String selectedAttributeAlgorithm = "CfsSubsetEval+BestFirst";
+    private static Classifier loadedClassifier = null;
 
     public static Instances buildInstances(){
         try {
@@ -25,7 +26,9 @@ public class Builder {
 
     public static Classifier loadClassifier(){
         try {
-            return (Classifier) SerializationHelper.read("models/" + selectedClassifier + "_" + selectedAttributeAlgorithm + ".model");
+            if(loadedClassifier==null)
+                loadedClassifier = (Classifier) SerializationHelper.read("models/" + selectedClassifier + "_" + selectedAttributeAlgorithm + ".model");
+            return loadedClassifier;
         }catch (Exception e){
             e.printStackTrace();
         }
